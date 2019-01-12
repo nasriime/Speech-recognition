b5b7577ac469073010b0efe838c3f1aaf127768c
nasriime <mnasr4040@gmail.com>
2019-01-12 16:36:18 +0200

    remoove build folder from .ignore file

---
+++ build/precache-manifest.b7eb7f2b5f2c99fbc2567fa0b77e36cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9eec2210140c64739d6e",
+    "url": "/static/js/main.9eec2210.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "0938ee83754967cd333094583eb9bf5b",
+    "url": "/index.html"
+  }
+];
