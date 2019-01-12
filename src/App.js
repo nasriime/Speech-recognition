778afbf644f2a5588c99e4add2b8961a8cda9c02
nasriime <mnasr4040@gmail.com>
2019-01-12 16:18:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Speech  from './Speech'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <Speech/>
+      </div>
+    );
+  }
+}
+
+export default App;
